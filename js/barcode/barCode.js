1485d6f456ebd2bd276cc8bf1674d84e426eec80
ohjenssen <oskar_jenssen@hotmail.com>
2024-09-02 09:09:58 +0200

    barcode

--- js/barcode/barCode.js
+++ js/barcode/barCode.js
@@ -1,17 +1,26 @@
-    // //https://wiki.openfoodfacts.org/API/Read
-    
-    // //Søger efter produktet med stregkoden: 5741000224427
-    // fetch(`https://dk.openfoodfacts.org/api/v0/product/5741000224427.json`)
-    //     .then(response => response.json())
-    //     .then(json => {
-    //         console.log(json);
-    //     });
-    
-    // //Søger efter produkter med "cola" i navnet
-    // fetch(`https://dk.openfoodfacts.org/cgi/search.pl?search_terms=cola&search_simple=1&action=process&json=1`)
-    //     .then(response => response.json())
-    //     .then(json => {
-    //         console.log(json);
-    //     });
+import {BarcodeDetector} from "https://fastly.jsdelivr.net/npm/barcode-detector@2/dist/es/pure.min.js";
+	
+const video = document.querySelector('#video');
+const resultNode = document.querySelector('#result');
+const stream = await navigator.mediaDevices.getUserMedia({video: {facingMode: 'environment'}});
+const barcodeDetector = new BarcodeDetector({
+    formats: ["qr_code", "code_128", "code_39"],
+});
+
+video.srcObject = stream;
+video.onloadedmetadata = () => {
+    video.play();
+    requestAnimationFrame(scanBarcode);
+};
+
+async function scanBarcode() {
+    const barcodes = await barcodeDetector.detect(video);
+    if(barcodes.length > 0) {
+        resultNode.innerText = `Stregkode fundet: ${barcodes[0].rawValue}`;
+        video.pause(); // Stop scanning
+    } else {
+        requestAnimationFrame(scanBarcode);
+    }
+}
 
     console.log('barcode')
