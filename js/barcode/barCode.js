26c6a033f9799743ebd3938b6ec30a84ba0138e7
ohjenssen <oskar_jenssen@hotmail.com>
2024-09-02 09:01:46 +0200

    Project setup

---
+++ js/barcode/barCode.js
@@ -0,0 +1,17 @@
+    // //https://wiki.openfoodfacts.org/API/Read
+    
+    // //Søger efter produktet med stregkoden: 5741000224427
+    // fetch(`https://dk.openfoodfacts.org/api/v0/product/5741000224427.json`)
+    //     .then(response => response.json())
+    //     .then(json => {
+    //         console.log(json);
+    //     });
+    
+    // //Søger efter produkter med "cola" i navnet
+    // fetch(`https://dk.openfoodfacts.org/cgi/search.pl?search_terms=cola&search_simple=1&action=process&json=1`)
+    //     .then(response => response.json())
+    //     .then(json => {
+    //         console.log(json);
+    //     });
+
+    console.log('barcode')
---
+++ js/index.js
@@ -0,0 +1,11 @@
+const path = location.pathname;
+
+switch(path){
+    case '/pages/barcode.html':
+        console.log('barcode');
+        break;
+
+    case '/pages/foodFacts.html':
+        console.log('Food facts');
+        break;
+}
