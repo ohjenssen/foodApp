180f872f7b141cff4af6c1a526c70933e03fd050
ohjenssen <oskar_jenssen@hotmail.com>
2024-09-04 12:14:56 +0200

    Omskrevet seemore så den ikke lenger behøver å gjøre et api kall

--- js/functions/createSeeMore.js
+++ js/functions/createSeeMore.js
@@ -2,26 +2,23 @@ export async function createSeeMoreBtn(product, card){
     const seeMoreBtn = document.createElement('button');
     seeMoreBtn.className = 'seeMoreBtn';
     seeMoreBtn.textContent = 'Se mer';
-    seeMoreBtn.dataset.productId = product.id;
 
     const productInfoDiv = card.querySelector('.productInfo');
     productInfoDiv.appendChild(seeMoreBtn);
 
-    seeMoreBtn.addEventListener('click', async () => {
+    seeMoreBtn.addEventListener('click', () => {
         seeMoreBtn.innerHTML = `<div class="dotLoader"></div>`;
-        const response = await fetch(`https://dk.openfoodfacts.org/api/v0/product/${product.id}.json`);
-        const productData = await response.json();
         seeMoreBtn.innerHTML = `Se mer`;
 
-        console.log(productData.product);
+        console.log(product);
+
         const modalTitle = document.querySelector('#productModal .modal-title');
         const modalBody = document.querySelector('#productModal .modal-body');
+        modalTitle.textContent = product.product_name;
 
-        modalTitle.textContent = productData.product.product_name;
-
-        modalBody.innerHTML = `
+          modalBody.innerHTML = `
             <div class="modalImgContainer">
-                <img src="${productData.product.image_front_url}" alt="${productData.product.product_name}" class="modalImg">
+                <img src="${product.image_front_url}" alt="${product.product_name}" class="modalImg">
             </div>
             <div class="nutriments">
                 <div class="nutrimentCategories">
@@ -33,12 +30,12 @@ export async function createSeeMoreBtn(product, card){
                     <p>Salt: </p>
                 </div>
                 <div class="nutrimentValues">
-                    <p>${productData.product.nutriscore_data.energy}</p>
-                    <p>${productData.product.nutriscore_data.saturated_fat}</p>
-                    <p>${productData.product.nutriscore_data.sugars}</p>
-                    <p>${productData.product.nutriscore_data.sugars}</p>
-                    <p>${productData.product.nutriscore_data.proteins}</p>
-                    <p>${productData.product.nutriscore_data.sodium}</p>
+                    <p>${product.nutriscore_data ? product.nutriscore_data.energy : "Ingen information"}</p>
+                    <p>${product.nutriscore_data ? product.nutriscore_data.saturated_fat : "Ingen information"}</p>
+                    <p>${product.nutriscore_data ? product.nutriscore_data.sugars : "Ingen information"}</p>
+                    <p>${product.nutriscore_data ? product.nutriscore_data.sugars : "Ingen information"}</p>
+                    <p>${product.nutriscore_data ? product.nutriscore_data.proteins : "Ingen information"}</p>
+                    <p>${product.nutriscore_data ? product.nutriscore_data.sodium : "Ingen information"}</p>
                 </div>
             </div>
         `;
